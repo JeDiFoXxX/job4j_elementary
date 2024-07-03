6bbf7010b5938c810a5efdcb348ecc7882d3bbbc
JeDiFoXxX <serjik1993991@gmail.com>
2024-07-03 19:33:05 +0300

    123

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -7,6 +7,7 @@ public class ChessBoard {
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
                 result =  Math.abs(y2 - y1);
+
             }
         }
         return result;

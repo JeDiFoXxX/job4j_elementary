51cd6afaffbbf9d54a3a552568048b09f4961968
JeDiFoXxX <serjik1993991@gmail.com>
2024-07-07 12:02:33 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] arrayShort = new short[10];
         String[] arrayString = new String[100500];
         float[] arrayFloat = new float[40];
+
+        System.out.println("Размер массива short равен: " + arrayShort.length);
+        System.out.println("Размер массива String равен: " + arrayString.length);
+        System.out.println("Размер массива float равен: " + arrayFloat.length);
     }
 }
